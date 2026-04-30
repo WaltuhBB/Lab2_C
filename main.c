4f16d1f65fc5d355e30309b7dceb91bb22a71002
Kirill <salatmail86@gmail.com>
2026-04-30 22:15:30 +0700

    added_clearMemory

--- main.c
+++ main.c
@@ -3,49 +3,96 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+//Создание матрицы и заполнение значениями из диапазона
 int** createMatrix_r(size_t row, size_t col, int down, int up)
 {
-
     int **ptrM = (int**)calloc(row, sizeof(int*));
 
-    int val = down;
-    for (size_t i = 0; i < row; i++)
+    if (ptrM != NULL)
     {
-        ptrM[i] = (int*)calloc(col, sizeof(int));
-
-        for (size_t j = 0; j < col; j++)
+        int val = down;
+        bool flagN = false;
+        
+        for (size_t i = 0; i < row && !flagN; i++)
         {
-            ptrM[i][j] = val;
-            val++;
+            ptrM[i] = (int*)calloc(col, sizeof(int));
 
-            if (val > up)
+            if (ptrM[i] != NULL)
             {
-                val = down;
+                for (size_t j = 0; j < col; j++)
+                {
+                    ptrM[i][j] = val;
+                    val++;
+
+                    if (val > up)
+                    {
+                        val = down;
+                    }
+                }
+            }
+            else
+            {
+                for (size_t k = 0; k < i; k++)
+                {
+                    free(ptrM[k]);
+                }
+                free(ptrM);
+                ptrM = NULL;
+                
+                flagN = true;
             }
         }
+
     }
 
     return ptrM;
+}
 
+//Очистка памяти динамической матрицы
+bool clearMemory(int** ptrM, size_t row)
+{
+    bool res = false;
+
+    if (ptrM != NULL)
+    {
+        for (size_t i = 0; i < row; i++)
+        {
+            free(ptrM[i]);
+        }
+
+        free(ptrM);
+        res = true;
+    }
+
+    return res;
 }
 
 int main()
 {
     
-    int **d_Mat = createMatrix_r(3, 3, 0, 3);
+    size_t row = 3;
+    size_t col = 3;
 
-    for (int i = 0; i < 3; i++)
+    int **d_Mat = createMatrix_r(row, col, 0, 3);
+
+    if (d_Mat != NULL)
     {
-        for (int j = 0; j < 3; j++)
+        for (int i = 0; i < 3; i++)
         {
-            printf("%d ", d_Mat[i][j]);
+            for (int j = 0; j < 3; j++)
+            {
+                printf("%d ", d_Mat[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
-
-        free(d_Mat[i]);
+    }
+    else
+    {
+        printf("Memory was never allocated ");
     }
 
-    free(d_Mat);
+    clearMemory(d_Mat, row);
 
     return 0;
+
 }
